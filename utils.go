135b39c10e234fe5e8e29c2c53043bd4c1b4ffe9
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-12-22 03:55:37 +0200

    add integer checker

--- utils.go
+++ utils.go
@@ -1,9 +1,17 @@
 package aoc
 
+import "math"
+
 func Sum[N Number](sl []N) N {
 	var sum N
 	for _, v := range sl {
 		sum += v
 	}
 	return sum
+}
+
+var Epsilon = 0.0001
+
+func IsInteger(f float64) bool {
+	return math.Abs(f-math.Round(f)) < Epsilon
 }
